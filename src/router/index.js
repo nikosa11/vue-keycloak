4309a96c068159d207d14668228b7efd46d74a92
nlitsakis <nlitsakis@gmail.com>
2024-06-12 18:39:33 +0300

    profile and home updated

--- src/router/index.js
+++ src/router/index.js
@@ -14,11 +14,6 @@ const router = createRouter({
                     component: () => import('@/views/Dashboard.vue')
                 },
                 {
-                    path: '/uikit/formlayout',
-                    name: 'formlayout',
-                    component: () => import('@/views/uikit/FormLayout.vue')
-                },
-                {
                     path: '/uikit/input',
                     name: 'input',
                     component: () => import('@/views/uikit/Input.vue')
